94019797e4cef82fd204ef3c8e8f8f5b48f0f6e2
Jordan Castillo <jordan.castillo@roadster.com>
2020-02-21 21:56:47 -0800

    initial ui setup with Vue

---
+++ ui/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { ClientState } from '../../../shared/models/client-state';
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    msg: String
+  }
+  // type inference enabled
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
---
+++ ui/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is blab. A bad chat app.</h1>
+  </div>
+</template>
