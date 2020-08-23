6480b4d70b7b6fc01c28d2841e84eb1c2c3b32ca
jccdev <jcastillodev@gmail.com>
2020-08-23 16:09:26 -0700

    added channels routes

--- blab-api/src/app.ts
+++ blab-api/src/app.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import users from './routes/users';
 import auth from './routes/auth';
+import channels from './routes/channels';
 
 const app = express();
 const port = 3001;
@@ -12,6 +13,6 @@ app.use(cors({ origin: "http://localhost:8080", methods: "GET,HEAD,PUT,PATCH,POS
 
 app.use(users);
 app.use(auth);
+app.use(channels)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
- 
