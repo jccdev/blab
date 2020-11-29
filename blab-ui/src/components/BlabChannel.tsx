4f0b88ac47ed3917c6db33e32dd9eb1a107f6182
jordan <jcastillodev@gmail.com>
2020-11-29 17:16:58 -0600

    Updated NPM packages and fixed issues with updates

--- blab-ui/src/components/BlabChannel.tsx
+++ blab-ui/src/components/BlabChannel.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 function BlabChannel(): JSX.Element {
-  const { name } = useParams();
+  const { name } = useParams<{ name: string }>();
   return <div>Channel: {name}</div>;
 }
 
